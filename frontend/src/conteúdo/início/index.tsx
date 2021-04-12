66013459eabd29a9514957af94adf9bfbba12f69
Henrique <henriqueborsoilopes@gmail.com>
2021-04-12 14:30:38 -0300

    Add imagens

--- frontend/src/conteúdo/início/index.tsx
+++ frontend/src/conteúdo/início/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Rodape from '../../rodapé';
+import { ReactComponent as ImgBody } from './imagens/vampiro.svg';
 import './styles.css';
 
 export default function Inicio() {
@@ -7,8 +8,7 @@ export default function Inicio() {
     return (
         <>
             <div className="container" id="bodyInicio">
-                
-                
+                {/* <ImgBody/> */}
             </div>
             <Rodape/>
         </>
